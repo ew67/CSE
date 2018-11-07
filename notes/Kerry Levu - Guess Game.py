c1a74bdad02a593f1da0ba47ac382994ad65e2e6
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-07 08:28:51 -0800

    Guess Game Finished

--- notes/Kerry Levu - Guess Game.py
+++ notes/Kerry Levu - Guess Game.py
@@ -2,20 +2,27 @@ import random
 random_int = (random.randint(1, 10))
 print(random_int)
 
-guesses = 0
-amount_guesses = guesses + 1
+
+guesses_left = 5
+Win = False
 
 
-
-for i in (1, 2, 3, 4, 5):
+while guesses_left > 0 and Win == False:
     print("What number am I thinking of?")
     guess = input()
     guess = int(guess)
     if guess > random_int:
         print("Go lower.")
+        guesses_left -= 1
     elif guess < random_int:
         print("Go higher.")
+        guesses_left -= 1
     elif guess == random_int:
         print("How did you figure that out? You must've cheated.")
+        Win = True
 
+
+
+if Win == False:
+    print("You've lost. Try again buddy.")
 
