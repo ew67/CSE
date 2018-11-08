886cafa5e65af31a41abbd616ccdee475dd950bf
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-08 08:42:53 -0800

    Lucky 7's 95% Done

--- notes/Kerry Levu - Lucky 7's.py
+++ notes/Kerry Levu - Lucky 7's.py
@@ -1,13 +1,28 @@
 import random
 
-dice_int_1 = (random.randint(1, 12))
-dice_int_2 = (random.randint(1, 12))
-
-
 money = 15
+money = int(money)
 
 amount_of_rounds = 0
 
+
 while money > 0:
-    print(random.randint(1, 12))
-    if random.randint == 7:
+    dice_int_1 = (random.randint(1, 6))
+    dice_int_2 = (random.randint(1, 6))
+    dice_roll = dice_int_1 + dice_int_2
+    print(dice_roll)
+    money -= 1
+
+    if dice_roll == 7:
+        money += 4
+        print("You have $%d right now!" % money)
+        amount_of_rounds += 1
+    else:
+        money -= 1
+        print("You have $%d right now!" % money)
+        amount_of_rounds += 1
+
+print()
+print()
+
+print(amount_of_rounds)
