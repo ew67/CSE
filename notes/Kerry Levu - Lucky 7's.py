788b80d80ca5c04cbbfee4f80c89d7ed37f331f3
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-09 09:47:24 -0800

    Progress

--- notes/Kerry Levu - Lucky 7's.py
+++ notes/Kerry Levu - Lucky 7's.py
@@ -3,14 +3,18 @@ import random
 money = 15
 money = int(money)
 
+
+highest_money = list()
+
 amount_of_rounds = 0
 
 
 while money > 0:
+    highest_money.append(money)
     dice_int_1 = (random.randint(1, 6))
     dice_int_2 = (random.randint(1, 6))
     dice_roll = dice_int_1 + dice_int_2
-    print(dice_roll)
+    print("Dice roll : %d" % dice_roll)
     money -= 1
 
     if dice_roll == 7:
@@ -18,11 +22,19 @@ while money > 0:
         print("You have $%d right now!" % money)
         amount_of_rounds += 1
     else:
-        money -= 1
         print("You have $%d right now!" % money)
         amount_of_rounds += 1
 
 print()
 print()
 
-print(amount_of_rounds)
+print("You lasted %d bets until you hit rock bottom." % amount_of_rounds)
+print()
+
+print(highest_money)
+
+print(len(highest_money))
+
+print(max(highest_money))
+
+print("The highest amount of money you had was %d" % highest_money)
