f2cc1aef8a4d488ac6bbd05b511a3fc93af75d15
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-13 08:45:02 -0800

    Challenge One Done - Lucky 7's

--- notes/Kerry Levu - Lucky 7's.py
+++ notes/Kerry Levu - Lucky 7's.py
@@ -4,13 +4,14 @@ money = 15
 money = int(money)
 
 
-highest_money = list()
+highest_money_list = list()
 
 amount_of_rounds = 0
-
+max_money = 0
+max_money = int(max_money)
 
 while money > 0:
-    highest_money.append(money)
+    highest_money_list.append(money)
     dice_int_1 = (random.randint(1, 6))
     dice_int_2 = (random.randint(1, 6))
     dice_roll = dice_int_1 + dice_int_2
@@ -18,23 +19,26 @@ while money > 0:
     money -= 1
 
     if dice_roll == 7:
-        money += 4
+        money += 5
         print("You have $%d right now!" % money)
         amount_of_rounds += 1
-    else:
-        print("You have $%d right now!" % money)
-        amount_of_rounds += 1
-
+        if max_money < money:
+            max_money = money
+    else: print("You have $%d right now!" % money)
+    amount_of_rounds += 1
 print()
 print()
 
 print("You lasted %d bets until you hit rock bottom." % amount_of_rounds)
 print()
 
-print(highest_money)
+print(highest_money_list)
 
-print(len(highest_money))
+print(len(highest_money_list))
 
-print(max(highest_money))
+highest_money = (max(highest_money_list))
 
 print("The highest amount of money you had was %d" % highest_money)
+print(max_money)
+
+
