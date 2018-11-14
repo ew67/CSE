585458aaf71a70394ea96fdf9930a02c28a5477d
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-14 08:40:55 -0800

    Lucky 7's
    Lucky 7's Base Game Done
    Extension Done

--- notes/Kerry Levu - Lucky 7's.py
+++ notes/Kerry Levu - Lucky 7's.py
@@ -3,15 +3,12 @@ import random
 money = 15
 money = int(money)
 
-
-highest_money_list = list()
-
 amount_of_rounds = 0
 max_money = 0
 max_money = int(max_money)
+round_most_money = 0
 
 while money > 0:
-    highest_money_list.append(money)
     dice_int_1 = (random.randint(1, 6))
     dice_int_2 = (random.randint(1, 6))
     dice_roll = dice_int_1 + dice_int_2
@@ -22,23 +19,24 @@ while money > 0:
         money += 5
         print("You have $%d right now!" % money)
         amount_of_rounds += 1
+        print("You're on Round %d" % amount_of_rounds)
+        print()
         if max_money < money:
             max_money = money
-    else: print("You have $%d right now!" % money)
-    amount_of_rounds += 1
+            round_most_money = amount_of_rounds
+    else:
+        print("You have $%d right now!" % money)
+        amount_of_rounds += 1
+        print("You're on Round %d" % amount_of_rounds)
+        print()
+
+
 print()
 print()
 
 print("You lasted %d bets until you hit rock bottom." % amount_of_rounds)
 print()
 
-print(highest_money_list)
 
-print(len(highest_money_list))
-
-highest_money = (max(highest_money_list))
-
-print("The highest amount of money you had was %d" % highest_money)
-print(max_money)
-
+print("At round %d you had $%d! You probably should've stopped right there." % (round_most_money, max_money))
 
