872cf3ae3bb9bdac80ddfb1a54df504b679c73b0
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-27 08:44:50 -0800

    List Notes

--- notes/List Notes.py
+++ notes/List Notes.py
@@ -32,3 +32,58 @@ print(new_list[1:3])
 print(new_list[1:4])  # 1:4 goes up to the indices but doesn't include the last one
 print(new_list[1:])  # Would include everything from Index 1 to the very end.
 print(new_list[:2])  # This is the same thing, but it would go from the start, and only up to Index 2.
+
+# Adding things to a list
+holiday_list = []  # Always use Brackets for Lists
+holiday_list.append("Tacos")
+holiday_list.append("Bumblebee")
+holiday_list.append("Red Dead Redemption 2")
+print(holiday_list)
+
+#  Notice this is "object.method(Parameters)"
+
+# Remove things from a lst
+holiday_list.remove("Tacos")
+print(holiday_list)
+
+# List Practice
+
+'''
+1. Make a new list with 3 items
+2. Add a 4th item to the lis
+3 Remove one of the first three items from the list.
+'''
+
+gift_list = ["Toys","Games","Roblox $10 Gift Card"]
+gift_list.append("Food")
+gift_list.remove("Toys")
+print(gift_list)
+
+gift_list.pop(0)  # Pop is like remove, but removes the item in the Index Number Instead
+print(gift_list)
+
+# Tuple
+brands = ("Apple", "Samsung", "HTC")  # notice the parentheses
+colors = ["blue", "red", "green", "black", "brown", "purple", "pink", "orange", "teal", "white", "yellow", "indigo",
+          "violet", "magenta", "gray", "cyan",
+          "tan", "lime", "Jade", "Silver"]
+print(len(colors))
+
+
+# Find the index
+print(colors.index("violet"))
+
+# Changing things into a list
+string1 = "Jade"
+list1 = list(string1)
+print(list1)
+
+for character in list1:
+    if character == "u":
+        # replace with a *
+        current_index = list1.index(character)
+        list1.pop(current_index)
+        list1.insert(current_index, "*")
+
+# Changing lists into strings
+print("".join(list1))
