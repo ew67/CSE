2fc6d63459a2aebf01b992dd6109d348fd79d731
ew67 <42383510+ew67@users.noreply.github.com>
2018-11-29 08:43:03 -0800

    Hangman Progress

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -3,8 +3,26 @@ import random
 random_answer_words_list = ["purple", "blue", "dog", "cat", "sky", "biology", "edison", "clovis", "black", "white",
                             "ghosts", "hangman"]
 
+answer_word = (random.choice(random_answer_words_list))
+string1 = answer_word
+answer_in_list = list(answer_word)
 
+# print(answer_word)  # Debug to see Answer.
+
+
+# Custom Word Options - Delete # to enable.
 # custom_word_answer = []
 # custom_word_answer.append(input("You want to play your own game, with your own word? Well, put it here then."))
 
-print(random.choice(list1))
+# while answer_in_list
+
+player_guess = input("What's your guess?")
+
+for character in answer_in_list:
+    if character == player_guess:
+        # replace with a *
+        current_index = answer_in_list.index(character)
+        answer_in_list.pop(current_index)
+        answer_in_list.insert(current_index, "*")
+
+print(answer_in_list)
