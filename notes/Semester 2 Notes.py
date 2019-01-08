c14502a2bcb111bd2dcccb54dcc4814d12a1cfee
Kerry L <ew67@fresnou.org>
2019-01-08 08:42:56 -0800

    Created Semester 2 Notes

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+# hola me llamo estevan
+
+cars = 5
+driving = True
+# cars = 5 → "5"
+
+print("tengo %d cars." % cars)
+print("I have " + str(cars) + " cars")
+
+# Age = input("How old are you?")
+# print("Lmao, you're %s? I belong in a museum." % Age)
+
+color_list = ["black", "grey", "blue", "red", "purple", "green", "yellow", "white"]
+color_list.append("brown")
+print(len(color_list))
+print(color_list)
+
+color_list.pop(0)
+print(color_list)
+    
