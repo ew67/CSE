8fd584c78843524f1438f3520a7ff3833de08ab8
Kerry L <ew67@fresnou.org>
2019-01-15 08:42:24 -0800

    Update Hangman.py

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -5,8 +5,9 @@ random_answer_words_list = ["purple", "blue", "dog", "cat", "sky", "biology", "e
                             "Bookshelf", "Vietnamese", "Japanese", "American", "Korean", "Chinese", "Polish", "Mexican",
                             "Laos", "Hispanic", "Spike", "Set", "Pass", "Molten", "Intel", "AMD", "Nvidia", "Steam",
                             "Battlefield", "New Year , New Me", ]
-
-answer_word = (random.choice(random_answer_words_list))
+answer_word = "boba"
+# Remove After
+# answer_word = (random.choice(random_answer_words_list))
 string1 = answer_word
 answer_in_list = list(answer_word)
 answer_hidden = answer_word
@@ -14,25 +15,28 @@ guesses_left = 8
 letters_used = []
 answer_in_list_joined = "".join(answer_in_list)
 
-# print(answer_word)
-# Delete # to enable.
 
-# Custom Word Options - Delete # to enable.
-# custom_word_answer = []
-# custom_word_answer.append(input("You want to play your own game, with your own word? Well, put it here then."))
+for i in range(len(answer_word)):
+    print("This word has %s letters." % len(answer_word))
+    print()
+    player_guess = input("What's your guess?")
+    letters_used.append(player_guess)
 
-# while answer_in_list
+    if player_guess in answer_in_list:
+        print("That's the right letter!")
+        current_index = answer_in_list.index(player_guess)
+    elif player_guess not in answer_in_list:
+        guesses_left -= 1
+    print("You have %d guesses left." % guesses_left)
 
-player_guess = input("What's your guess?")
-letters_used.append(player_guess)
 
-
-for character in answer_in_list:
+'''for character in answer_in_list:
     if character == player_guess:
         # replace with a *
         current_index = answer_in_list.index(character)
         answer_in_list.pop(current_index)
         answer_in_list.insert(current_index, "a")
+'''
 
 print(answer_in_list)
 print(answer_in_list_joined)
