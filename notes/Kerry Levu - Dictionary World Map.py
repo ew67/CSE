0b88e1c83d23e639d7cfea9be9f993c799976e8b
Kerry L <ew67@fresnou.org>
2019-01-31 08:43:06 -0800

    Update Kerry Levu - Dictionary World Map.py

--- notes/Kerry Levu - Dictionary World Map.py
+++ notes/Kerry Levu - Dictionary World Map.py
@@ -8,7 +8,7 @@ world_map = {
 
         }
     },
-    'PARKING_lOT': {
+    'PARKING_LOT': {
         'NAME': "The Edison Parking Lot",
         'DECSCRIPTION': "These are cars parked here. To "
                         "the south is Mr. Wiebe's room",
@@ -19,5 +19,22 @@ world_map = {
     }
 }
 
+# Other Variables
 
+current_node = world_map['R19A']
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN", ]
+playing = True
+
+# Controller
+while playing:
+    print(current_node["NAME"])
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
 
