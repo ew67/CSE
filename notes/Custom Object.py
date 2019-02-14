a34c023c4fc803fa0ea8341fb33ca0e284e57b06
Kerry L <ew67@fresnou.org>
2019-02-14 08:44:05 -0800

    Update Custom Object.py

--- notes/Custom Object.py
+++ notes/Custom Object.py
@@ -1,5 +1,33 @@
-class Water_Bottle:
-    def __init__(self):
-        self.capacity
-        self.cap
-        self.color
+import random
+
+class Waterbottle:
+    def __init__(self, amountin=100, capacity=100, cap=False, color="black", rate_of_fire=5,
+                 opacity="transparent", material="plastic"):
+        self.capacity = capacity
+        self.cap = cap
+        self.color = color
+        self.rate_of_fire = rate_of_fire
+        self.opacity = opacity
+        self.material = material
+        self.amountin = amountin
+
+    def refill(self):
+        self.amountin = 100
+    def empty(self):
+        self.amountin = 0
+    def consume(self):
+        print("You have %d in the water bottle." % self.amountin)
+        want = input("How much do you want to drink?")
+        want = int(want)
+        amount_left = self.amountin - want
+        print("You have %d in the water bottle." % amount_left)
+    # def bottle_flip:
+    #     random.randint(1,6)
+
+
+
+kyle_waterbottle = Waterbottle(100, 10, False, "green", 1, "opaque", "steel")
+
+print(kyle_waterbottle)
+
+kyle_waterbottle.consume()
