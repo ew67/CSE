8c6311ee8fcc319b2cf91171a83284abf27a6715
Kerry L <ew67@fresnou.org>
2019-02-27 08:43:53 -0800

    Update World Map OOP.py

--- notes/World Map OOP.py
+++ notes/World Map OOP.py
@@ -8,6 +8,16 @@ class Room(object):
         self.up = up
         self.down = down
         self.description = description
+        self.character = []
+
+
+class Monster(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.current_location = starting_location
+
+
+
 
 
 class Player(object):
@@ -16,12 +26,12 @@ class Player(object):
         self.current_location = starting_location
         self.inventory = []
 
-    def move(self, newlocation):
+    def move(self, new_location):
         """This method moves a character to a new location
 
-        :param newlocation: The variable containing a room object
+        :param new_location: The variable containing a room object
         """
-        self.current_location = newlocation
+        self.current_location = new_location
 
 
 # Option 1
@@ -42,44 +52,45 @@ playing = True
 
 MAIN_DRIVEWAY = Room("Main Driveway", 'HOUSE_GARAGE', None, None, None, None, None,
                      "You're outside. There are cars in front of you. The garage is slightly opened.")
-HOUSE_GARAGE = Room("Garage", 'WASHING_ROOM', MAIN_DRIVEWAY, None, None, None, None, "The garage is empty.")
-WASHING_ROOM = Room("Washing Room", 'DOWNSTAIRS_HALLWAY', HOUSE_GARAGE, None, None, None, None,
-                    "There's a dryer and washing machine. Nearby a cabinent is open.")
-DOWNSTAIRS_HALLWAY = Room("Downstairs Hallway", 'MONIQUE_ROOM', WASHING_ROOM, 'DOWNSTAIRS_HALLWAY_CONT',
+HOUSE_GARAGE = Room("Garage", 'WASHING_ROOM', 'MAIN_DRIVEWAY', None, None, None, None, "The garage is empty.")
+WASHING_ROOM = Room("Washing Room", 'DOWNSTAIRS_HALLWAY', 'HOUSE_GARAGE', None, None, None, None,
+                    "There's a dryer and washing machine. Nearby a cabinet is open.")
+DOWNSTAIRS_HALLWAY = Room("Downstairs Hallway", 'MONIQUE_ROOM', 'WASHING_ROOM', 'DOWNSTAIRS_HALLWAY_CONT',
                           'KENDRICK_ROOM', None, None, "You're in a hallway. There's an old painting on the wall. "
                                                        "There's a door to your left. The hallway continues West.")
-KENDRICK_ROOM = Room("Kendrick's Room", None, None, DOWNSTAIRS_HALLWAY, None, None, None, "The room looks normal, but "
-                                                                                          "the wooden floors creak.")
+KENDRICK_ROOM = Room("Kendrick's Room", None, None, 'DOWNSTAIRS_HALLWAY', None, None, None,
+                     "The room looks normal, but the wooden floors creak.")
 MONIQUE_ROOM = Room("Monique's Room", None, 'DOWNSTAIRS_HALLWAY_CONT', None, None, None, None,
                     "You came through the wall. There'" + 's' + "and old keycard with 2581 printed on it.")
-DOWNSTAIRS_HALLWAY_CONT = Room("Downstairs Hallway", MONIQUE_ROOM, 'DOWNSTAIRS_BATHROOM', 'DOWNSTAIRS_LIVING_ROOM',
-                               DOWNSTAIRS_HALLWAY, None, None,
+DOWNSTAIRS_HALLWAY_CONT = Room("Downstairs Hallway", 'MONIQUE_ROOM', 'DOWNSTAIRS_BATHROOM', 'DOWNSTAIRS_LIVING_ROOM',
+                               'DOWNSTAIRS_HALLWAY', None, None,
                                "You're still in the hallway. There is a room in front and behind you.")
-DOWNSTAIRS_BATHROOM = Room("Downstairs Bathroom", DOWNSTAIRS_HALLWAY_CONT, None, None, None, None, None,
+DOWNSTAIRS_BATHROOM = Room("Downstairs Bathroom", 'DOWNSTAIRS_HALLWAY_CONT', None, None, None, None, None,
                            "It smells really bad.")
-DOWNSTAIRS_LIVING_ROOM = Room("Downstairs Living Room", None, 'STAIRWELL', 'DINING_ROOM', DOWNSTAIRS_HALLWAY_CONT, None,
-                              None, "You're in a Living Room.")
-DINING_ROOM = Room("Dining Room", None, 'KITCHEN', None, DOWNSTAIRS_LIVING_ROOM, None, None, "There's fresh food "
-                                                                                             "lying on the table!")
-KITCHEN = Room("Kitchen", DINING_ROOM, None, None, None, None, None, "Here lies more food. A few fruits and raw fish. "
-                                                                     "There's a knife. ")
-DOWNSTAIRS_STAIRWELL = Room("Downstairs Stairwell", DOWNSTAIRS_LIVING_ROOM, None, None, None, 'UPSTAIRS_STAIRWELL')
-UPSTAIRS_STAIRWELL = Room("Upstairs Stairwell", 'UPSTAIRS_HALLWAY', None, None, None, None, DOWNSTAIRS_STAIRWELL)
-UPSTAIRS_HALLWAY = Room("Main Upstairs Hallway", 'MASTER_BED_ROOM', UPSTAIRS_STAIRWELL,
+DOWNSTAIRS_LIVING_ROOM = Room("Downstairs Living Room", None, 'DOWNSTAIRS_STAIRWELL', 'DINING_ROOM',
+                              'DOWNSTAIRS_HALLWAY_CONT', None, None, "You're in a Living Room.")
+DINING_ROOM = Room("Dining Room", None, 'KITCHEN', None, 'DOWNSTAIRS_LIVING_ROOM', None, None,
+                   "There's fresh food lying on the table!")
+KITCHEN = Room("Kitchen", 'DINING_ROOM', None, None, None, None, None,
+               "Here lies more food. A few fruits and raw fish. There's a knife. ")
+DOWNSTAIRS_STAIRWELL = Room("Downstairs Stairwell", 'DOWNSTAIRS_LIVING_ROOM', None, None, None, 'UPSTAIRS_STAIRWELL')
+UPSTAIRS_STAIRWELL = Room("Upstairs Stairwell", 'UPSTAIRS_HALLWAY', None, None, None, None, 'DOWNSTAIRS_STAIRWELL')
+UPSTAIRS_HALLWAY = Room("Main Upstairs Hallway", 'MASTER_BED_ROOM', 'UPSTAIRS_STAIRWELL',
                         'UPSTAIRS_HALLWAY_CONT', 'UPSTAIRS_LIVING_ROOM')
-UPSTAIRS_LIVING_ROOM = Room("Upstairs Living Room", None, None, UPSTAIRS_HALLWAY)
-MASTER_BED_ROOM = Room("Master Bedroom", 'MASTER_BALCONY', UPSTAIRS_HALLWAY, None, 'MASTER_HALLWAY')
-MASTER_BALCONY = Room("Upstairs Balcony", None, MASTER_BED_ROOM)
+UPSTAIRS_LIVING_ROOM = Room("Upstairs Living Room", None, None, 'UPSTAIRS_HALLWAY')
+MASTER_BED_ROOM = Room("Master Bedroom", 'MASTER_BALCONY', 'UPSTAIRS_HALLWAY', None, 'MASTER_HALLWAY')
+MASTER_BALCONY = Room("Upstairs Balcony", None, 'MASTER_BED_ROOM')
 MASTER_HALLWAY = Room("Master Hallway", 'MASTER_NORTH_CLOSET', 'MASTER_SOUTH_CLOSET', None, 'MASTER_BATHROOM')
-MASTER_NORTH_CLOSET = Room("North Closet", None, MASTER_HALLWAY)
-MASTER_SOUTH_CLOSET = Room("South Closet", MASTER_NORTH_CLOSET)
-MASTER_BATHROOM = Room("Master Bathroom", None, None, MASTER_HALLWAY)
-UPSTAIRS_HALLWAY_CONT = Room("East Upstairs Hallway", 'UPSTAIRS_HALLWAY_NORTH', None, None, UPSTAIRS_HALLWAY)
-UPSTAIRS_HALLWAY_NORTH = Room("North Upstairs Hallway", 'LILIE_ROOM', UPSTAIRS_HALLWAY_CONT, 'UPSTAIRS_BATHROOM')
-LILIE_ROOM = Room("Lilie's Room", None, UPSTAIRS_HALLWAY_NORTH)
+MASTER_NORTH_CLOSET = Room("North Closet", None, 'MASTER_HALLWAY')
+MASTER_SOUTH_CLOSET = Room("South Closet", 'MASTER_NORTH_CLOSET')
+MASTER_BATHROOM = Room("Master Bathroom", None, None, 'MASTER_HALLWAY')
+UPSTAIRS_HALLWAY_CONT = Room("East Upstairs Hallway", 'UPSTAIRS_HALLWAY_NORTH', None, None, 'UPSTAIRS_HALLWAY')
+UPSTAIRS_HALLWAY_NORTH = Room("North Upstairs Hallway", 'LILIE_ROOM', 'UPSTAIRS_HALLWAY_CONT', 'UPSTAIRS_BATHROOM')
+LILIE_ROOM = Room("Lilie's Room", None, 'UPSTAIRS_HALLWAY_NORTH')
 
 
 player = Player(MAIN_DRIVEWAY)
+Big_Scary_Man = Monster(DOWNSTAIRS_STAIRWELL)
 
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
 
@@ -93,11 +104,12 @@ while playing:
     elif command.lower() in directions:
         try:
             # command = 'north'
-            room_object = getattr(player.current_location, command)
-            # NEEDED FOR OPTION 2
-            room_var = globals()[room_object]
+            room_name = getattr(player.current_location, command)
+            room_object = globals()[room_name]
             player.move(room_object)
-        except KeyError:
+        except AttributeError:
             print("I can't go that way.")
+        except KeyError:
+            print("This key does not exist.")
     else:
         print("Command Not Recognized.")
