43bae2199df53d239368bec0412ed0c06b0b4fbe
Kerry L <ew67@fresnou.org>
2019-03-06 08:43:11 -0800

    Update Kerry Levu - Items.py

--- notes/Kerry Levu - Items.py
+++ notes/Kerry Levu - Items.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Item(object):
     def __init__(self, name, value):
         self.Name = name
@@ -7,11 +8,12 @@ class Item(object):
 
 
 class Weapon(Item):
-    def __init__(self, name, value, damage, element, role):
+    def __init__(self, name, value, damage, element, role, material):
         super(Weapon, self).__init__(name, value)
         self.damage = damage
         self.element = element
         self.role = role
+        self.material = material
 
     def attack(self):
         enemy_hp -= self.damage
@@ -25,15 +27,32 @@ class Weapon(Item):
         else:
             print("Lmao you dropped your Note 8 and the screen cracks. You die.")
 
+
 class Melee(Weapon):
+    def __init__(self, name, value, damage, element, role, material, handedness):
+        super(Melee, self).__init__(name, value, damage, element, role, material)
+        self.handedness = handedness
+
 
 class Ranged(Weapon):
-    def __init__(self, name, value, damage, element, role, ammo_type, weapon_range):
-        super(Ranged, self).__init__(name, value, damage, element, role)
+    def __init__(self, name, value, damage, element, role, material, ammo_type, weapon_range):
+        super(Ranged, self).__init__(name, value, damage, element, role, material)
         self.ammo_type = ammo_type
         self.weapon_range = weapon_range
 
 
-test_sword = Ranged("Bow of Dirt", 0, 0.5, "Neutral", "", Arrow, 2)
+class Sword(Melee):
+    def __init__(self, name, value, damage, element, role, material, sword_type, handedness):
+        super(Sword, self).__init__(name, value, damage, element, role, material, handedness)
+        self.sword_type = sword_type
 
-test_sword.attack()
+
+class Mace(Melee):
+    def __init__(self, name, value, damage, element, role, material, handedness):
+        super(Mace, self).__init__(name, value, damage, element, role, material, handedness)
+
+
+god_mace = Mace("Mac", "1500", 5, "Fire", "Knight", "Diamond", "1-Handed")
+
+
+god_mace.attack()
