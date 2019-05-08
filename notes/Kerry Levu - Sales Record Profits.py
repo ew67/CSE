af366d2b707e1e0574852aa8fe127d23c935690a
ew67 <42383510+ew67@users.noreply.github.com>
2019-05-08 08:43:57 -0700

    record profits

--- notes/Kerry Levu - Sales Record Profits.py
+++ notes/Kerry Levu - Sales Record Profits.py
@@ -1,16 +1,19 @@
 import csv
 
 
-# def check(num):
-
-
-
-
 with open("SalesRecords.csv", 'r') as old_csv:
     with open("SalesEdited.csv", 'w', newline='') as new_csv:
+        print("Processing...")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
-        item_type = row[2]
+        max_profit = [0, 0]
+        next(reader)
         for row in reader:
             print(row)
-
+            item_type = row[2]
+            profit = row[13]
+            if int(profit) > max_profit[0]:
+                max_profit.clear()
+                max_profit.insert(1, profit)
+            print(max_profit)
+        print("Done...")
